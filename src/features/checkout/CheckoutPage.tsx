9942c8db9660e134dc68da44f529a813a0e63ba7
Samir <Admin@SAMIR>
2022-03-08 00:12:28 +0100

    stripe payment functionailty integrated

--- src/features/checkout/CheckoutPage.tsx
+++ src/features/checkout/CheckoutPage.tsx
@@ -13,30 +13,54 @@ import { FieldValues, FormProvider, useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { validationSchema } from './CheckoutValidation';
 import agent from '../../app/api/agent';
-import { useAppDispatch } from '../../app/store/configureStore';
+import { useAppDispatch, useAppSelector } from '../../app/store/configureStore';
 import { clearBasket } from '../basket/basketSlice';
 import { LoadingButton } from '@mui/lab';
+import { StripeElementType } from '@stripe/stripe-js';
+import { useElements, CardNumberElement, useStripe } from '@stripe/react-stripe-js';
 
 const steps = ['Shipping address', 'Review your order', 'Payment details'];
 
-function getStepContent(step: number) {
-  switch (step) {
-    case 0:
-      return <AddressForm />;
-    case 1:
-      return <Review />;
-    case 2:
-      return <PaymentForm />;
-    default:
-      throw new Error('Unknown step');
-  }
-}
+
 
 export default function CheckoutPage() {
   const [orderNumber, setOrderNumber] = useState(0);
   const [activeStep, setActiveStep] = useState(0);
   const [loading, setLoading] = useState(false)
   const dispatch = useAppDispatch();
+  const [paymentMessage, setPaymentMessage] = useState('');
+  const [paymentSucceeded, setPaymentSucceeded] = useState(false);
+  const {basket} = useAppSelector(state => state.basket);
+  const stripe = useStripe();
+  const elements = useElements();
+  
+
+  const [cardState, setCardState] = useState<{elementError: {[key in StripeElementType]?: string}}>({elementError: {}});
+  const [cardComplete, setCardComplete] = useState<any>({cardNumber: false, cardExpiry: false, cardCvc: false});
+
+  const onCardInputChange = (event: any) => {
+    setCardState({
+      ...cardState,
+      elementError: {
+        ...cardState.elementError,
+        [event.elementType]: event.error?.message
+      }
+    })
+    setCardComplete({...cardComplete, [event.elementType]: event.complete})
+  }
+
+  function getStepContent(step: number) {
+    switch (step) {
+      case 0:
+        return <AddressForm />;
+      case 1:
+        return <Review />;
+      case 2:
+        return <PaymentForm cardState={cardState} onCardInputChange={onCardInputChange} />;
+      default:
+        throw new Error('Unknown step');
+    }
+  }
 
   const currentValidationSchema = validationSchema[activeStep]
 
@@ -54,20 +78,45 @@ export default function CheckoutPage() {
       })
   }, [methods])
 
-  const handleNext = async (data: FieldValues) => {
+  const submitOrder = async (data: FieldValues)=>{
+    setLoading(true);
     const {nameOnCard, saveAddress, ...shippingAddress} = data;
-    if(activeStep === steps.length - 1){
-      setLoading(true);
-      try{
+    if(!stripe || !elements) return;
+    try {
+      const cardElement = elements.getElement(CardNumberElement);
+      const paymentResult = await stripe.confirmCardPayment(basket?.clientSecret!, {
+        payment_method: {
+          card: cardElement!,
+          billing_details: {
+            name: nameOnCard
+          }
+        }
+      });
+      console.log(paymentResult);
+      if(paymentResult.paymentIntent?.status === "succeeded")
+      {
         const orderNumber = await agent.Orders.create({saveAddress, shippingAddress});
         setOrderNumber(orderNumber);
+        setPaymentSucceeded(true);
+        setPaymentMessage('Thank you--- we have received your payment');
         setActiveStep(activeStep + 1);
         dispatch(clearBasket());
         setLoading(false);
-      }catch(error){
-        console.log(error);
-        setLoading(false);        
+      }else{
+        setPaymentMessage(paymentResult.error?.message!);
+        setPaymentSucceeded(false);
+        setLoading(false);
+        setActiveStep(activeStep + 1)
       }
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  }
+
+  const handleNext = async (data: FieldValues) => {
+    if(activeStep === steps.length - 1){
+      await submitOrder(data);
     }else{
       setActiveStep(activeStep + 1);
     }
@@ -76,6 +125,15 @@ export default function CheckoutPage() {
   const handleBack = () => {
     setActiveStep(activeStep - 1);
   };
+
+  const submitDisabled = ()=>{
+    if(activeStep === steps.length - 1)
+    {
+      return !cardComplete.cardCvc || !cardComplete.cardExpiry || !cardComplete.cardNumber || !methods.formState.isValid;
+    }else{
+      return !methods.formState.isValid;
+    }
+  }
 
   return (
     <FormProvider {...methods}>
@@ -94,13 +152,19 @@ export default function CheckoutPage() {
           {activeStep === steps.length ? (
             <>
               <Typography variant="h5" gutterBottom>
-                Thank you for your order.
+                {paymentMessage}
               </Typography>
+              {paymentSucceeded ? (
               <Typography variant="subtitle1">
                 Your order number is #{orderNumber}. We have emailed your order
                 confirmation, and will not send you an update when your order has
                 shipped as this is all fake!
               </Typography>
+              ) : (
+                <Button variant='contained' onClick={handleBack}>
+                  Go back and try again
+                </Button>
+              )}
             </>
           ) : (
             <>
@@ -114,7 +178,7 @@ export default function CheckoutPage() {
                 <Button
                   variant="contained"
                   type='submit'
-                  disabled={!methods.formState.isValid}
+                  disabled={submitDisabled()}
                   sx={{ mt: 3, ml: 1 }}
                 >
                   {activeStep === steps.length - 1 ? 'Place order' : 'Next'}
---
+++ src/features/checkout/CheckoutWrapper.tsx
@@ -0,0 +1,31 @@
+import { Elements } from '@stripe/react-stripe-js'
+import { loadStripe } from '@stripe/stripe-js'
+import React, { useEffect, useState } from 'react'
+import agent from '../../app/api/agent';
+import LoadingComponent from '../../app/layout/LoadingComponent';
+import { useAppDispatch } from '../../app/store/configureStore';
+import { setBasket } from '../basket/basketSlice';
+import CheckoutPage from './CheckoutPage'
+
+const stripePromise = loadStripe("pk_test_51KagcSGEvbzaXqZ1yeBk9pMlsT77MLneH4jQ9TGoZn6ENfGcI8LEGZuJYKYRBr7z1rqgHw6X0NrQiXEUQWxoR0UK00fq2RzOG3");
+
+export default function CheckoutWrapper() {
+  const dispatch = useAppDispatch();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(()=>{
+    agent.Payments.createPaymentIntent()
+      .then((basket)=> dispatch(setBasket(basket)))
+      .catch(error => console.log(error))
+      .finally(()=> setLoading(false));
+  }, [dispatch])
+
+  if(loading) return <LoadingComponent message='Loading checkout...' />
+
+
+  return (
+    <Elements stripe={stripePromise}>
+        <CheckoutPage/>
+    </Elements>
+  )
+}
